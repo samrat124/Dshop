124c004b2349f0dcaf5ae42e9d5ab8fe07b6cb6d
samrat kumar <sam9999950@gmail.com>
2023-03-12 18:56:52 +0530

    Adding auth

--- dshop-client/src/Components/AllRoutes.jsx
+++ dshop-client/src/Components/AllRoutes.jsx
@@ -3,6 +3,7 @@ import {Routes,Route} from 'react-router-dom'
 import Category from './Category'
 import Login from './Login'
 import Product from './Product'
+// import Auth from './Auth'
 
 const AllRoutes = () => {
   return (
---
+++ dshop-client/src/Components/Auth.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { redirect, useNavigate } from 'react-router-dom';
+import { loginAction } from './Redux/Action'
+
+
+const Auth = (children) => {
+    // let navigate=useNavigate();
+    let login=useSelector((e)=>e.loginData);
+   if(login){
+    return children
+   }
+   else{
+    // navigate('/login')
+   }
+}
+
+export default Auth
