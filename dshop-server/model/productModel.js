715e6998a4534fac070141ab67fe56d5a825c902
samrat kumar <sam9999950@gmail.com>
2023-03-15 12:44:15 +0530

    working on models

--- dshop-server/model/productModel.js
+++ dshop-server/model/productModel.js
@@ -1,2 +1,13 @@
 const mongoose=require('mongoose');
 
+const productSchema=new mongoose.Schema({
+    title: String,
+    image: String,
+    category: String,
+    price: Number,
+    ratings: Number
+})
+
+const productModel=mongoose.model('product',productSchema)
+
+module.exports={productModel};
