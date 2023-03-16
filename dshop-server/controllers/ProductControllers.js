6e40763da12483ce04289cf52e06c15699b9efb0
samrat kumar <sam9999950@gmail.com>
2023-03-16 10:02:57 +0530

    working on controllers in server

--- dshop-server/controllers/ProductControllers.js
+++ dshop-server/controllers/ProductControllers.js
@@ -1,3 +1,11 @@
-exports.getAllProducts=(req,res)=>{
-    res.status(200).json({message:'server is working'})
+
+const product=require('../model/productModel')
+
+exports.getAllProducts=async(req,res,next)=>{
+
+    const products=await product.find()
+    res.status(200).json({message:'server is working',
+    success:true,
+    products,
+})
 }
